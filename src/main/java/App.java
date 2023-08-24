6b0228552ef054af5ff8d3758e31ac810a2af831
Vladimir <Idatiev08@gmail.com>
2023-08-24 13:06:47 +0300

    first

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,24 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld beanHelloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("first bean helloworld: " + beanHelloWorld.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("second bean helloworld: " + bean2.getMessage());
+
+        boolean checkBeanHelloWorld = beanHelloWorld == bean2;
+        System.out.println(checkBeanHelloWorld + ": " + beanHelloWorld + ", " + bean2);
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println("first bean cat: " + beanCat.getMessage());
+
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("second bean cat: " + beanCat2.getMessage());
+
+        boolean checkBeanCat = beanCat == beanCat2;
+        System.out.println(checkBeanCat + ": " + beanCat + ", " + beanCat2);
     }
 }
